def1a0a0ab4eeada2c9e5bad7779f03af1f8324a
Hanmin Kim <hanminkim123@gmail.com>
2024-07-06 20:05:52 -0700

    test(front/name_resolution): test name resolution

--- src/front/passes/name_resolution.rs
+++ src/front/passes/name_resolution.rs
@@ -30,3 +30,71 @@ impl NameResolver {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+    use crate::front::ast_types::{Definition, Reference, StructDef, Type, TypeReference, VarDef, VarReference};
+    use crate::modules::ModuleId;
+
+    #[test]
+    fn test_name_resolution() {
+        let module_id = ModuleId::from("module_a");
+        let mut definitions = vec![
+            Definition::StructDef(StructDef {
+                name: TypeReference::new("struct_a".to_string()),
+                field_types: HashMap::new(),
+            }),
+            Definition::StructDef(StructDef {
+                name: TypeReference::new("struct_b".to_string()),
+                field_types: {
+                    let mut field_types = HashMap::new();
+                    field_types.insert("field_a".to_string(), Type::Struct(TypeReference::new("struct_a".to_string())));
+                    field_types.insert("field_b".to_string(), Type::Int);
+                    field_types
+                },
+            }),
+            Definition::VarDef(VarDef {
+                name: VarReference::new("var_a".to_string()),
+                ty: Type::Struct(TypeReference::new("struct_b".to_string())),
+            }),
+        ];
+
+        let result = NameResolver::run(module_id.clone(), &mut definitions);
+        assert_eq!(result, Ok(()));
+
+        match definitions[0] {
+            Definition::StructDef(ref struct_def) => {
+                assert_eq!(struct_def.name.resolved, Some((module_id.clone(), "0:struct_a".to_string())));
+            }
+            _ => panic!("Expected StructDef"),
+        }
+
+        match definitions[1] {
+            Definition::StructDef(ref struct_def) => {
+                assert_eq!(struct_def.name.resolved, Some((module_id.clone(), "0:struct_b".to_string())));
+                match struct_def.field_types["field_a"] {
+                    Type::Struct(ref type_ref) => {
+                        assert_eq!(type_ref.resolved, Some((module_id.clone(), "0:struct_a".to_string())));
+                    }
+                    _ => panic!("Expected Struct"),
+                }
+            }
+            _ => panic!("Expected StructDef"),
+        }
+
+        match definitions[2] {
+            Definition::VarDef(ref var_def) => {
+                assert_eq!(var_def.name.resolved, Some((module_id.clone(), "0:var_a".to_string())));
+                match var_def.ty {
+                    Type::Struct(ref type_ref) => {
+                        assert_eq!(type_ref.resolved, Some((module_id.clone(), "0:struct_b".to_string())));
+                    }
+                    _ => panic!("Expected Struct"),
+                }
+            }
+            _ => panic!("Expected VarDef"),
+        }
+    }
+}
