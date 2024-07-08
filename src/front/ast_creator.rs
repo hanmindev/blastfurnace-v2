c75a78ca7821712e168786aa9ba17e2734c9ded6
Hanmin Kim <hanminkim123@gmail.com>
2024-07-07 18:11:40 -0700

    feat(front/ast_creator): add create ast function

--- src/front/ast_creator.rs
+++ src/front/ast_creator.rs
@@ -1,3 +1,14 @@
+use crate::front::ast_creator::lexer::get_tokens;
+use crate::front::ast_types::Module;
+
 mod lexer;
 mod token_types;
 mod parser;
+
+fn create_ast(src: &str) -> Module {
+    // TODO: error handling
+    let tokens = get_tokens(src).unwrap();
+    let ast = parser::parse_tokens(tokens).unwrap();
+
+    return ast;
+}
