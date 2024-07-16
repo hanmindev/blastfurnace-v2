4ca49b10b32fbc233fa8a13f5089a972601e85f8
Hanmin Kim <hanminkim123@gmail.com>
2024-07-15 20:19:59 -0700

    test(front): fix tests

--- src/front/ast_creator.rs
+++ src/front/ast_creator.rs
@@ -58,82 +58,83 @@ mod tests {
         assert_eq!(uses, ast.uses);
     }
 
-    // #[test]
-    // fn test_create_ast_struct() {
-    //     let src = r#"
-    //     struct struct_a {
-    //         field_a: int,
-    //         field_b: struct_b,
-    //     }
-    //     "#;
-    //
-    //     let expected_ast = ASTFile {
-    //         uses: UseMap {
-    //             uses: HashMap::new(),
-    //         },
-    //         definitions: vec![Definition::StructDef(StructDef {
-    //             name: TypeReference::new("struct_a".to_string()),
-    //             field_types: {
-    //                 let mut field_types = HashMap::new();
-    //                 field_types.insert("field_a".to_string(), Type::Int);
-    //                 field_types.insert(
-    //                     "field_b".to_string(),
-    //                     Type::Struct(TypeReference::new("struct_b".to_string())),
-    //                 );
-    //                 field_types
-    //             },
-    //         })],
-    //     };
-    //
-    //     let ast = create_ast(src);
-    //     assert_eq!(ast, expected_ast);
-    // }
-    //
-    // #[test]
-    // fn test_create_ast_static() {
-    //     let src = r#"
-    //     static val: int;
-    //     "#;
-    //
-    //     let expected_ast = ASTFile {
-    //         uses: UseMap {
-    //             uses: HashMap::new(),
-    //         },
-    //         definitions: vec![Definition::VarDef(VarDef {
-    //             name: VarReference::new("val".to_string()),
-    //             ty: Type::Int,
-    //         })],
-    //     };
-    // }
-    //
-    // #[test]
-    // fn test_create_ast_fn() {
-    //     let src = r#"
-    //     fn fn_a(arg_a: int, arg_b: struct_b) -> struct_c {
-    //     }
-    //     "#;
-    //
-    //     let expected_ast = ASTFile {
-    //         uses: UseMap {
-    //             uses: HashMap::new(),
-    //         },
-    //         definitions: vec![Definition::FnDef(FnDef {
-    //             return_type: Type::Struct(TypeReference::new("struct_c".to_string())),
-    //             name: FunctionReference::new("fn_a".to_string()),
-    //             args: vec![
-    //                 VarDef {
-    //                     name: VarReference::new("arg_a".to_string()),
-    //                     ty: Type::Int,
-    //                 },
-    //                 VarDef {
-    //                     name: VarReference::new("arg_b".to_string()),
-    //                     ty: Type::Struct(TypeReference::new("struct_b".to_string())),
-    //                 },
-    //             ],
-    //         })],
-    //     };
-    //
-    //     let ast = create_ast(src);
-    //     assert_eq!(ast, expected_ast);
-    // }
+    #[test]
+    fn test_create_ast_struct() {
+        let current_package = "package_a";
+        let current_module_path = Utf8PathBuf::from("foo\\bar");
+        let src = r#"
+        struct struct_a {
+            field_a: int,
+            field_b: struct_b,
+        }
+        "#;
+
+        let expected = ASTFile {
+            uses: vec![],
+            definitions: vec![Definition::StructDef(StructDef {
+                name: TypeReference::new("struct_a".to_string()),
+                field_types: {
+                    let mut field_types = HashMap::new();
+                    field_types.insert("field_a".to_string(), Type::Int);
+                    field_types.insert(
+                        "field_b".to_string(),
+                        Type::Struct(TypeReference::new("struct_b".to_string())),
+                    );
+                    field_types
+                },
+            })],
+        };
+
+        let ast = create_ast(current_package, src);
+        assert_eq!(expected, ast);
+    }
+
+    #[test]
+    fn test_create_ast_static() {
+        let current_package = "package_a";
+        let src = r#"
+        static val: int;
+        "#;
+
+        let expected_ast = ASTFile {
+            uses: vec![],
+            definitions: vec![Definition::VarDef(VarDef {
+                name: VarReference::new("val".to_string()),
+                ty: Type::Int,
+            })],
+        };
+
+        let ast = create_ast(current_package, src);
+        assert_eq!(expected_ast, ast);
+    }
+
+    #[test]
+    fn test_create_ast_fn() {
+        let current_package = "package_a";
+        let src = r#"
+        fn fn_a(arg_a: int, arg_b: struct_b) -> struct_c {
+        }
+        "#;
+
+        let expected_ast = ASTFile {
+            uses: vec![],
+            definitions: vec![Definition::FnDef(FnDef {
+                return_type: Type::Struct(TypeReference::new("struct_c".to_string())),
+                name: FunctionReference::new("fn_a".to_string()),
+                args: vec![
+                    VarDef {
+                        name: VarReference::new("arg_a".to_string()),
+                        ty: Type::Int,
+                    },
+                    VarDef {
+                        name: VarReference::new("arg_b".to_string()),
+                        ty: Type::Struct(TypeReference::new("struct_b".to_string())),
+                    },
+                ],
+            })],
+        };
+
+        let ast = create_ast(current_package, src);
+        assert_eq!(expected_ast, ast);
+    }
 }
