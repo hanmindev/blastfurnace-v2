77ea72033dc907098f5693a8e55587d028ab40ff
Hanmin Kim <hanminkim123@gmail.com>
2024-07-20 00:33:19 -0700

    feat: add dependency collection

--- src/front.rs
+++ src/front.rs
@@ -7,23 +7,21 @@ use crate::front::ast_creator::create_ast;
 use crate::front::ast_types::FullItemPath;
 use crate::front::definition_table::DefinitionTable;
 use crate::front::passes::collect_definitions::collect_definitions;
+use crate::front::passes::collect_dependencies::collect_dependencies;
 use crate::front::passes::name_resolution::resolve_names;
-use crate::modules::ModuleDependencies;
-use std::collections::HashSet;
+use crate::modules::{module_id_from_local, ModuleDependencies};
 
 pub fn parse_file(
     module_path: FullItemPath,
     file_contents: &str,
 ) -> (ModuleDependencies, DefinitionTable) {
+    let module_id = module_id_from_local(&module_path.package_name, &module_path.item_path);
     let mut module = create_ast(&module_path.package_name, file_contents);
 
     // TODO: error handling
     resolve_names(module_path, &mut module).unwrap();
-
-    // TODO: get this information
-    let _module_dependencies: ModuleDependencies = HashSet::new();
-
-    let definition_table = collect_definitions(&mut module);
+    let _module_dependencies: ModuleDependencies = collect_dependencies(module_id, &mut module);
+    let _definition_table = collect_definitions(&mut module);
 
     todo!()
 }
--- src/front/passes.rs
+++ src/front/passes.rs
@@ -1,3 +1,4 @@
 pub mod collect_definitions;
+pub mod collect_dependencies;
 pub mod name_resolution;
 mod visitor;
